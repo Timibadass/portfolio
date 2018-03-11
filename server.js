3f2227bc4f5c58e6df2de42bb8638ae60d44bf7f
= <=>
2018-03-11 06:14:43 +0100

    little changes to the server

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('*', (req, res) => {
-    res.sendfile(__dirname  + 'dist/index.html');
+    res.sendfile(__dirname  + '/dist/index.html');
 });
